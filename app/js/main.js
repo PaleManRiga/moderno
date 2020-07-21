688dc4a6b43c58c14611f08f69341e385825bd6f
Sergejs <sergejs_rudenko@inbox.lv>
2020-07-21 13:05:38 +0300

    added new items

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,18 @@ $(function () {
 
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function(){
+        
+    })
 
     var mixer = mixitup('.products__inner-box');
 
